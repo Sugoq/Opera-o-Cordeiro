075e59912ee84a61574cacbaf00d627c06c73922
Sugoq <pedrohcherubini@gmail.com>
2022-08-10 00:08:13 -0300

    Changing Player Collider

--- Assets/Scripts/GroundCheck.cs
+++ Assets/Scripts/GroundCheck.cs
@@ -4,12 +4,27 @@ public class GroundCheck : MonoBehaviour
 {
     public bool isGrounded;
 
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
+
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        print("Colidindo");
+        isGrounded = true;
+    }
+
     private void OnTriggerStay2D(Collider2D collision)
     {
         isGrounded = true;
     }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         isGrounded = false;
     }
 }
+
